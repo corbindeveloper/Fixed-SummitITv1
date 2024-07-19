d03e7b9aebd81452b9714d3a4ec8c97203791594
StarGitHub16 <114962374+StarGitHub16@users.noreply.github.com>
2024-07-19 11:29:06 -0500

    Removing the repo due to an error

---
+++ js/script.js
@@ -0,0 +1,114 @@
+//Navigation Scroll Functionality 
+window.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementById("stickyHeader")
+    let lastScrollY = 0;
+
+    window.addEventListener( 'scroll', () => {
+        const currentScrollY = window.scrollY
+
+        if(currentScrollY > lastScrollY) {
+            header.classList.add('hidden');
+            header.style.pointerEvents = "none";
+        } else {
+            header.classList.remove('hidden')
+            header.style.display = "flex";
+            header.style.pointerEvents = "all";
+        } 
+        lastScrollY = currentScrollY;
+        }); 
+    })
+
+
+
+//Dropdown Cards Functionality
+
+const toggleButtons = document.querySelectorAll(".toggleButton");
+
+toggleButtons.forEach(button => {
+    button.addEventListener("click", function() {
+        const answer = this.nextElementSibling;
+        const icon = this.querySelector("i");
+        const circle = this.querySelector("div.plus-circle");
+
+        if(answer.style.display === "block") {
+            answer.style.display = "none";
+            icon.classList.remove("fa-minus");
+            icon.classList.add("fa-plus");
+            circle.style.backgroundColor ="#BF2026";
+        }
+        else {
+            answer.style.display = "block";
+            circle.style.backgroundColor = "#36454F";
+            icon.classList.remove("fa-plus");
+            icon.classList.add("fa-minus");
+            
+        }
+    })
+})
+
+
+
+
+//Show More functionality 
+  function showMore() {
+    let featureHide = document.getElementById("feature-hide");
+
+    //Hide the div after clicking on it once.
+    let showHide = document.querySelector('div.show-more')
+
+
+    if(featureHide.style.display == "flex") {
+      featureHide.style.display = "none";
+      
+    } else
+    {
+      
+      featureHide.style.display = "flex";
+      showHide.style.display ="none";
+    }  
+}
+                
+//Mobile Navbar Functionality Functions
+function mobileMenu() {
+  const mobileNavList = document.getElementById('mobile-nav-func')
+
+  const body = document.querySelector("body");
+
+  if(mobileNavList.style.display === "flex") {
+    mobileNavList.style.display = "none";
+    body.style.removeProperty('overflow'); 
+  } else {
+    mobileNavList.style.display = "flex";
+    body.style.overflow = "hidden";
+  }
+}
+
+function mobileAbout() {
+  const mobileAboutList = document.getElementById('about-nav')
+
+  if(mobileAboutList.style.display === "flex") {
+    mobileAboutList.style.display = "none";
+  } else {
+    mobileAboutList.style.display = 'flex';
+  }
+}
+
+function mobileService() {
+  const mobileServiceList = document.getElementById('service-nav')
+
+  if(mobileServiceList.style.display === "flex") {
+    mobileServiceList.style.display = "none";
+  } else {
+    mobileServiceList.style.display = 'flex';
+  }
+}
+
+//Annoucement Bar closing functionality 
+const announcementBar = document.getElementById('announcement-bar');
+const closeButton = document.getElementById('announcement-close');
+const mainContent = document.getElementById('main-content');
+
+closeButton.addEventListener('click', () => {
+  announcementBar.style.display = 'none';
+  mainContent.style.paddingTop = '0px';
+});
